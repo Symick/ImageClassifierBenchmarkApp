b6d758bc5f767ed87ca8143b51e900fba8b844ec
Julian Kruithof <julian.kruithof@moveagency.com>
2024-10-25 15:13:29 +0200

    initial commit

---
+++ app/src/main/java/com/example/aibatteryusagebenchmark/ui/BenchmarkApp.kt
@@ -0,0 +1,10 @@
+package com.example.aibatteryusagebenchmark.ui
+
+import android.app.Application
+
+class BenchmarkApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
